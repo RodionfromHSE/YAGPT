12b4986b3f9dfbe4037bb2d9228a514a24d0db83
zj-karina <rkarina2703@gmail.com>
2023-04-02 17:41:50 +0000

    some corrections, all other other changes please commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 params = {
-    'data_path': 'encoded_texts.pkl',
+    'data_path': './encoded_texts.pkl',
     'checkpoint': "AlexWortega/instruct_rugptMedium",
     'additional_special_tokens': [f'speaker{i}: ' for i in range(1, 10)] + ['history: '],
     'num_proc': 5
