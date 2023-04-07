a2fa6d1a79200b07672bb93ea08d07f2e3120479
RodionfromHSE <rodionkhvorostov@gmail.com>
2023-04-07 23:48:43 +0200

    IGoToBed

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ params = {
     'data_path': 'data.csv',
     'checkpoint': "AlexWortega/instruct_rugptMedium",
     'additional_special_tokens': [f'speaker{i}: ' for i in range(1, 10)] + ['history: '],
-    'num_proc': 5,
+    'num_proc': 10,
     'dataset_name': "under-tree/prepared-yagpt"
 }
