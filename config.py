18216854ff0a67e4656f13e432cd49092e402483
RodionfromHSE <rodionkhvorostov@gmail.com>
2023-04-07 18:07:11 +0200

    updates

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ params = {
     'data_path': 'data.csv',
     'checkpoint': "AlexWortega/instruct_rugptMedium",
     'additional_special_tokens': [f'speaker{i}: ' for i in range(1, 10)] + ['history: '],
-    'num_proc': 5
+    'num_proc': 5,
+    'dataset_name': "under-tree/prepared-yagpt"
 }
